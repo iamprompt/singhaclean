f786c36a56bce119bbb9f5cdab83e32cd0132eba
Supakarn Laorattanakul <wearedprompt@gmail.com>
2024-05-04 15:16:35 +0700

    feat: add machines count

---
+++ src/app/api/stats/machines/route.ts
@@ -0,0 +1,23 @@
+import { MachineHistory } from '@/db/models'
+
+export const GET = async (req: Request) => {
+  const data = await MachineHistory.aggregate([
+    { $group: { _id: '$machine', count: { $sum: 1 } } },
+    {
+      $lookup: {
+        from: 'machines',
+        localField: '_id',
+        foreignField: '_id',
+        as: 'machine',
+      },
+    },
+    { $unwind: { path: '$machine', preserveNullAndEmptyArrays: true } },
+    {
+      $replaceRoot: {
+        newRoot: { $mergeObjects: ['$machine', { count: '$count' }] },
+      },
+    },
+  ])
+
+  return Response.json(data)
+}
