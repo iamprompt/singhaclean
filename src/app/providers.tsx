6df05cfd4f6b09f7fa36411fdbedf352b54c0333
Supakarn Laorattanakul <wearedprompt@gmail.com>
2024-05-05 23:51:58 +0700

    fix: avoid refetch on focus

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
